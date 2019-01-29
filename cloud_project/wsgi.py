6f233b97d680eb1e40b189010b3074f7e3485cca
Mayank Thakur <thakurmayank88@gmail.com>
2019-01-29 15:57:07 +0530

    Not working wsgi

--- cloud_project/wsgi.py
+++ cloud_project/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cloud_project.settings')
+sys.path.append('/home/mayank/PES/Sem6/CC/cloud/')
+sys.path.append('/home/mayank/PES/Sem6/CC/selfie-less-act/')
+sys.path.append('/home/mayank/PES/Sem6/CC/selfie-less-act/cloud_project')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cloud_project.settings'
 
 application = get_wsgi_application()
