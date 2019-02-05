8a218bd391f9f3f8c6ff591c6e5af9d55d2871a2
Mayank Thakur <thakurmayank88@gmail.com>
2019-02-05 19:36:32 +0530

    Added models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Category, Act
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Act)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# Category: (catId, categoryName)
+# Act: (actId, url, upvote, timestamp, caption, category)
+
+class Category(models.Model):
+	category_name = models.CharField(max_length=128, unique=True)
+	def __str__(self):
+		return self.category_name
+
+class Act(models.Model):
+	image = models.ImageField()
+	upvote = models.IntegerField()
+	timestamp = models.DateTimeField(auto_now=True)
+	caption = models.CharField(max_length=512)
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
