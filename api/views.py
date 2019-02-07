2b1bbefea6525068bfde33e338b280270278ecf5
Mayank Thakur <thakurmayank88@gmail.com>
2019-02-07 12:37:33 +0530

    Completed Template

--- api/views.py
+++ api/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import io
+from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework import generics
+from rest_framework import status
 
 from .serializers import AddUserSerializer
 from .request import AddUserRequest
+
+from .models import User
 # Create your views here.
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def add_user_view(request):
     if request.method == 'POST':
-        sth = AddUserSerializer(request)
-        print(sth.username)
-        # print(sth.data)
-        return Response("hii")
+        sth = AddUserSerializer(data=request.data)
+        print(sth.is_valid())
+        print(sth['username'].value)
+        user = User(username= sth['username'].value, password=sth['password'].value)
+        user.save()
+        print(sth.is_valid())
+        print(sth.validated_data)
+        r = JSONRenderer()
+        data = r.render(dict())
+        return Response(data=data, status=status.HTTP_201_CREATED)
