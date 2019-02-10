aa8d501a614a9a24417519fdae4f01bfc242a515
megha-09 <meghakalal09@gmail.com>
2019-02-10 14:55:49 +0530

    7 api

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name='api'
 urlpatterns = [
     path('users/', views.add_user_view, name='add_user'),
+    path('acts/<int:act_id>/', views.remove_act,name='remove_act'),
+    path('acts/',views.upload_an_act, name='upload_an_act'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 import io
 from rest_framework.parsers import JSONParser
@@ -11,9 +12,13 @@ from rest_framework import generics
 from rest_framework import status
 
 from .serializers import AddUserSerializer
+from .serializers import AddCategorySerializer
 from .request import AddUserRequest
 
 from .models import User
+from .models import Act
+from .models import Category
+
 # Create your views here.
 @api_view(['POST'])
 def add_user_view(request):
@@ -28,3 +33,38 @@ def add_user_view(request):
         r = JSONRenderer()
         data = r.render(dict())
         return Response(data=data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['DELETE'])
+def remove_act(request,act_id):
+	if request.method=='DELETE':
+		try:
+			instance=Act.object.get(pk=act_id)
+			instance.delete()
+			return Response(data={}, status=status.HTTP_200_OK)
+		except:
+			return Response(data={}, status= status.HTTP_400_BAD_REQUEST)
+
+	else:
+		return Response(data={}, status= status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+
+
+
+
+
+
+	
+
+		
+
+			
+
+
+
+
+
+
+
+
